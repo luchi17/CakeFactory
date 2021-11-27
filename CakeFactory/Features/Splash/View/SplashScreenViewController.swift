4b0aacad933cf0b110a46140b8d700022389d780
Mariluz Parejo Alcázar <mariluz.parejo@movetia.com>
2021-11-27 13:48:56 +0100

    SplashScreen UI

--- CakeFactory/Features/Splash/View/SplashScreenViewController.swift
+++ CakeFactory/Features/Splash/View/SplashScreenViewController.swift
@@ -11,12 +11,16 @@ import UIKit
 
 class SplashScreenViewController: UIViewController {
     
+    @IBOutlet private weak var contentView: UIView!
     @IBOutlet private weak var titleLabel: UILabel!
     
     private var viewModel = SplashScreenViewModel()
     
     override func viewDidLoad() {
-        self.view.backgroundColor = UIColor(named: "F6A5EC")
+        
+        super.viewDidLoad()
+        
+        self.contentView.backgroundColor = UIColor(red: 246/257, green: 165/257, blue: 236/257, alpha: 1.0)
         self.titleLabel.text = "Cake Factory"
         self.titleLabel.textColor = .white
         
