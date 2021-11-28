14dce413f6ccede34274923a3c323eb904e853e3
Mariluz Parejo Alcázar <mariluz.parejo@movetia.com>
2021-11-28 14:22:38 +0100

    Cake list viewController and CollectionView protocols implemented

---
+++ CakeFactory/Features/CakeList/View/CakeListViewController.swift
@@ -0,0 +1,88 @@
+//
+//  CakeListViewController.swift
+//  CakeFactory
+//
+//  Created by Mariluz Parejo on 27/11/21.
+//
+
+import UIKit
+
+class CakeListViewController: UIViewController {
+
+    @IBOutlet private weak var collectionView: UICollectionView!
+    @IBOutlet private weak var updateListButton: UIButton!
+    
+    private var viewModel: CakeListInterfaceToViewModelProtocol = CakeListViewModel()
+    
+    private var cakeListModel: CakeListModel = CakeListModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+        
+        viewModel.loadCakeList()
+        
+        viewModel.bindCakeListModel = { list in
+            guard let list = list else { return }
+            let uniqueItems = Array(Set(list.items))
+            let uniqueItemsSorted = uniqueItems.sorted {$0.title < $1.title}
+            
+            self.cakeListModel.items = uniqueItemsSorted
+            self.collectionView.reloadData()
+        }
+    }
+    
+    func configureUI() {
+        
+        updateListButton.setTitle("Update list", for: .normal)
+        updateListButton.setTitleColor(.black, for: .normal)
+        
+        let itemWidth: CGFloat = view.frame.width * 0.40
+        let collectionViewLayout = UICollectionViewFlowLayout()
+        collectionViewLayout.itemSize = CGSize(width: itemWidth, height: 100.0)
+        collectionViewLayout.minimumLineSpacing = 15
+        collectionViewLayout.scrollDirection = .vertical
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.decelerationRate = UIScrollView.DecelerationRate.fast
+        collectionView.collectionViewLayout = collectionViewLayout
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = UIColor(red: 246/257, green: 165/257, blue: 236/257, alpha: 1.0).withAlphaComponent(0.4)
+        
+        let cakeCollectionViewCellIdentifier = String(describing: CakeCollectionViewCell.self)
+        let cakeCollectionViewCellNib = UINib(nibName: cakeCollectionViewCellIdentifier, bundle: nil)
+        collectionView.register(cakeCollectionViewCellNib, forCellWithReuseIdentifier: cakeCollectionViewCellIdentifier)
+    }
+
+}
+
+extension CakeListViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension CakeListViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cakeItem = self.cakeListModel.items[indexPath.row]
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: CakeCollectionViewCell.self), for: indexPath) as? CakeCollectionViewCell {
+            
+            cell.configureCell(model: cakeItem)
+            return cell
+        }
+        
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.cakeListModel.items.count
+    }
+    
+    
+}
