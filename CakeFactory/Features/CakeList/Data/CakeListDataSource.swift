d4c9bab4e7df8620a0f01beb575aab25d9f1411a
Mariluz Parejo Alcázar <mariluz.parejo@movetia.com>
2021-11-28 21:56:31 +0100

    Handle service error in data and domain

--- CakeFactory/Features/CakeList/Data/CakeListDataSource.swift
+++ CakeFactory/Features/CakeList/Data/CakeListDataSource.swift
@@ -11,16 +11,20 @@ class CakeListDataSource : NSObject, CakeListDataSourceProtocol {
     
     private let sourcesURL = URL(string: "https://gist.githubusercontent.com/juananthony/c51c635c877d53d0fbc7d803f23af7ea/raw/0d4454a75859e8f94834a3de257b0b69a77e0b10/cakes")!
     
-    func getCakeListData(completion: @escaping ([CakeItemDataOut]) -> ()) {
+    func getCakeListData(completion: @escaping ([CakeItemDataOut]?) -> ()) {
         
         DispatchQueue.main.async {
             URLSession.shared.dataTask(with: self.sourcesURL) { (data, response, error) in
-                if let data = data {
-                    
-                    let jsonDecoder = JSONDecoder()
-                    
-                    let cakeData = try! jsonDecoder.decode([CakeItemDataOut].self, from: data)
-                    completion(cakeData)
+                if error != nil || data == nil {
+                        completion(nil)
+                } else {
+                    if let data = data {
+
+                        let jsonDecoder = JSONDecoder()
+
+                        let cakeData = try? jsonDecoder.decode([CakeItemDataOut].self, from: data)
+                        completion(cakeData)
+                    }
                 }
             }.resume()
         }
@@ -28,5 +32,5 @@ class CakeListDataSource : NSObject, CakeListDataSourceProtocol {
 }
 
 protocol CakeListDataSourceProtocol {
-    func getCakeListData(completion: @escaping ([CakeItemDataOut]) -> ())
+    func getCakeListData(completion: @escaping ([CakeItemDataOut]?) -> ())
 }
--- CakeFactory/Features/CakeList/Domain/CakeListManager.swift
+++ CakeFactory/Features/CakeList/Domain/CakeListManager.swift
@@ -14,23 +14,28 @@ class CakeListManager: NSObject, CakeListManagerProtocol {
         self.dataSource = dataSource
     }
     
-    func getCakeListData(completion: @escaping (CakeListModel) -> ()) {
+    func getCakeListData(completion: @escaping (CakeListModel?) -> ()) {
         self.dataSource.getCakeListData { items in
-            var cakeItems: [CakeItemModel] = []
-        
-            for itemDataOut in items {
-                let cakeItem = CakeItemModel(title: itemDataOut.title,
-                                             description: itemDataOut.desc,
-                                             imageUrlString: itemDataOut.image)
-                cakeItems.append(cakeItem)
-            }
             
-            let cakeListModel = CakeListModel(items: cakeItems)
-            completion(cakeListModel)
+            if let items = items {
+                var cakeItems: [CakeItemModel] = []
+            
+                for itemDataOut in items {
+                    let cakeItem = CakeItemModel(title: itemDataOut.title,
+                                                 description: itemDataOut.desc,
+                                                 imageUrlString: itemDataOut.image)
+                    cakeItems.append(cakeItem)
+                }
+                
+                let cakeListModel = CakeListModel(items: cakeItems)
+                completion(cakeListModel)
+            } else {
+                completion(nil)
+            }
         }
     }
 }
 
 protocol CakeListManagerProtocol {
-    func getCakeListData(completion: @escaping (CakeListModel) -> ())
+    func getCakeListData(completion: @escaping (CakeListModel?) -> ())
 }
